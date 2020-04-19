a503655423d28dfbdf8a65ad52d9c7ec41236bdf
Kim Gabon <45309584+KimGabs@users.noreply.github.com>
2020-04-19 10:52:45 +0800

    initial commit

--- url_parser.py
+++ url_parser.py
@@ -1,86 +1,82 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-#import webbrowser
+import os
 
-def get_soup(url):
-
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'}
-
-    r = s.get(url, headers=headers)
-
-    #with open('temp.html', 'wb') as f:
-    #    f.write(r.content)
-    #    webbrowser.open('temp.html')
-
-    if r.status_code != 200:
-        print('status code:', r.status_code)
-    else:
-        return BeautifulSoup(r.text, 'html.parser')
-
-def parse(url, response):
-
-    if not response:
-        print('no response:', url)
-        return
-
-    # get number of reviews
-    num_reviews = response.find('span', class_='reviews_header_count').text
-    num_reviews = num_reviews[1:-1] # remove `( )`
-    num_reviews = num_reviews.replace(',', '') # remove `,`
-    num_reviews = int(num_reviews)
-    print('num_reviews:', num_reviews, type(num_reviews))
-
-    # create template for urls to pages with reviews
-    url = url.replace('.html', '-or{}.html')
-    print('template:', url)
-
-    # load pages with reviews
-    for offset in range(0, num_reviews, 5):
-        print('url:', url.format(offset))
-        url_ = url.format(offset)
-        parse_reviews(url_, get_soup(url_))
-        return # for test only - to stop after first page
-
-def parse_reviews(url, response):
-    print('review:', url)
-
-    if not response:
-        print('no response:', url)
-        return
-
-    # get every review
-    for idx, review in enumerate(response.find_all('div', class_='review-container')):
-        item = {
-            'hotel_name': response.find('h1', class_='heading_title').text,
-            'review_title': review.find('span', class_='noQuotes').text,
-            'review_body': review.find('p', class_='partial_entry').text,
-            'review_date': review.find('span', class_='relativeDate')['title'],#.text,#[idx],
-            'num_reviews_reviewer': review.find('span', class_='badgetext').text,
-            'reviewer_name': review.find('span', class_='scrname').text,
-            'bubble_rating': review.select_one('div.reviewItemInline span.ui_bubble_rating')['class'][1][7:],
-        }
-
-        results.append(item) # <--- add to global list
-
-        #~ yield item
-        for key,val in item.items():
-            print(key, ':', val)
-        print('----')
-        #return # for test only - to stop after first review
+results = []
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763'
+headers = {'User-Agent': user_agent}
 
 
-# --- main ---
+class ParseTripAdvisor:
+    def __init__(self, x):
+        self.hotel_name = ''
+        self.limit_reviews = x
+        self.num_reviews = 0
+        self.reviewer_name = ''
+        self.review_body = ''
+        self.info = {}
+        self.r = None
 
-s = requests.Session()
+    def ifHotel_Exist(self):
+        PATH = './' + self.hotel_name + '.csv'
+        if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
+            print("File ", self.hotel_name, " exists and is readable for update")
+            return True
+        else:
+            print("The file does not exist...\n", 'Creating new file...')
+            return False
 
-start_urls = [
-    'https://www.tripadvisor.com/Hotel_Review-g562819-d289642-Reviews-Hotel_Caserio-Playa_del_Ingles_Maspalomas_Gran_Canaria_Canary_Islands.html',
-    #'https://www.tripadvisor.com/Hotel_Review-g60795-d102542-Reviews-Courtyard_Philadelphia_Airport-Philadelphia_Pennsylvania.html',
-    #'https://www.tripadvisor.com/Hotel_Review-g60795-d122332-Reviews-The_Ritz_Carlton_Philadelphia-Philadelphia_Pennsylvania.html',
-]
+    def get_soup(self, url):
+        s = requests.Session()
+        self.r = s.get(url, headers=headers)
+        if self.r.status_code != 200:
+            print('status code:', self.r.status_code)
+        else:
+            soup = BeautifulSoup(self.r.text, 'html.parser')
+            return soup
 
-results = [] # <--- global list for items
+    def parse(self, url, response):
+        self.check_response(url, response)
+        # Get hotel name
+        self.hotel_name = response.find('h1', id='HEADING').text
+        self.num_reviews = self.get_num_reviews(response)
+        # Limit number of reviews to output
+        self.limit_num_reviews()
+        # print(self.limit_reviews, ' ', self.num_reviews, '\n')
+        # create template for urls to pages with reviews
+        url = url.replace('.html', '-or{}.html')
+        # load pages with reviews
+        for offset in range(0, self.limit_reviews, 5):
+            # print('url:', url.format(offset))
+            url_ = url.format(offset)
+            self.parse_reviews(url_, self.get_soup(url_))
+            # return  # for test only - to stop after first page
+        return self.hotel_name
 
-for url in start_urls:
-    parse(url, get_soup(url))
+    def parse_reviews(self, url, response):
+            self.check_response(url, response)
+            # get every review
+            for idx, review in enumerate(response.find_all('div', {'data-test-target': 'HR_CC_CARD'})):
+                # Get reviewer name
+                self.reviewer_name = review.find(
+                    'a', {'class': 'ui_header_link social-member-event-MemberEventOnObjectBlock__member--35-jC'}).text
+
+                # Get the body content of user review without clicking 'read more'
+                p = review.find('div', {'class': 'cPQsENeY'}).find('q')
+                self.review_body = ''
+                for child in p.children:
+                    if child.name == "span":
+                        self.review_body += child.text
+                    elif child.name == 'None':
+                        self.review_body += child.string.rstrip("\"\n ").lstrip("\"\n ")
+
+
+                self.info = {
+                    'hotel': self.hotel_name,
+                    'reviewer': self.reviewer_name,
+                    # 'review_title': review_title,
+                    'review': self.review_body
+                }
+
+                results.append(self.info)
+                # return # for test only - to stop after first review

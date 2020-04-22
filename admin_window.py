3e90e33ecafba38345a7441738355ee6b12cb1c8
Kim Gabon <45309584+KimGabs@users.noreply.github.com>
2020-04-22 19:11:34 +0800

    Update admin_window.py

--- admin_window.py
+++ admin_window.py
@@ -7,99 +7,149 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-import codes.url_parser 
+from PyQt5.QtCore import pyqtSignal
+import codes.url_parser
 from codes.url_parser import *
 from codes.Filter_Reviews import *
 from data_Conversion import *
+import time
+import socket
 
-class Ui_MainWindow(object):
 
-    def start(self):
+class QThread2(QtCore.QThread):
+    sig2 = pyqtSignal(str))
+
+    def __init__(self, parent = None):
+        QtCore.Qthread.__init__(self, parent)
+
+    def get_ip(self):
+        hostname=socket.gethostname()
+        IPAddr=socket.gethostbyname(hostname)
+        self.sig2.emit('Your Computer IP Address is: ' + IPAddr)
+
+
+
+class QThread1(QtCore.QThread):
+
+    sig1=pyqtSignal(str)
+
+    def __init__(self, parent = None):
+        QtCore.QThread.__init__(self, parent)
+
+    def on_source(self, lineftxt):
+        self.source_txt=lineftxt
+
+    def run(self):
         global results
-        url = self.lineEdit.text()
-        pt = ParseTripAdvisor(20)
-        pt.get_soup(url)
-        pt.parse(url)
-        self.plainTextEdit.insertPlainText(pt.hotel_name + '\n')
-        self.QtCore.QTimer.singleShot(0, self.start)
-        # if check == True:
-        #     self.plainTextEdit.insertPlainText("File "+ p.hotel_name +" already exists and is readable\n")
-        #     pass
-        # else:
-        #     self.plainTextEdit.insertPlainText("The file does not exist...\n"+ "Creating new file...\n")
-        #     df = to_DataFrame(results)  # Convert list to DataFrame
-        #     del results[:]
-        #     df = filter_review(df, p.hotel_name)  # Divide the review sentences from the DataFrame
-        #     self.plainTextEdit.insertPlainText(df + '\n')
-        #     saveto_sql(df, p.hotel_name)  # <---- save pandas to sql
-        #     self.plainTextEdit.insertPlainText(p.hotel_name + ' added to sql\n')
-        #     saveto_csv(df, p.hotel_name)  # <---- save pandas to sql
-        #     self.plainTextEdit.insertPlainText(p.hotel_name + ' added to csv\n')
+        pt=ParseTripAdvisor(20)
+        self.running=True
+        while self.running:
+            try:
+                pt.get_soup(self.source_txt)
+                pt.parse(self.source_txt)
+                self.sig1.emit(pt.hotelName)
+                time.sleep(1)
+                if pt.HotelExist == True:
+                    self.sig1.emit(str(pt.hotelName) + 'already exists')
+                else:
+                    self.sig1.emit(str('Creating new file...'))
+                    time.sleep(1)
+                    df=to_DataFrame(results)
+                    del results[:]
+                    df=filter_review(df, pt.hotelName)
+                    time.sleep(2)
+                    self.sig1.emit(str(df))
+                    time.sleep(1)
+                    SetSave(df, pt.hotelName)
+                    break
+            except Exception as err:
+                self.sig1.emit(str(err))
+
+
+class Ui_MainWindow(QtWidgets.QMainWindow):
+
+    sig=pyqtSignal(str)
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(480, 367)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget=QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.verticalLayout_6 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_6=QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout_6.setObjectName("verticalLayout_6")
-        self.gridLayout_2 = QtWidgets.QGridLayout()
+        self.gridLayout_2=QtWidgets.QGridLayout()
         self.gridLayout_2.setObjectName("gridLayout_2")
-        
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+
+        self.pushButton=QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setObjectName("pushButton")
         self.gridLayout_2.addWidget(self.pushButton, 1, 0, 1, 1)
 
-        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2=QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.gridLayout_2.addWidget(self.lineEdit_2, 0, 1, 1, 1)
-        
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+
+        self.lineEdit=QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setText("")
         self.lineEdit.setObjectName("lineEdit")
         self.gridLayout_2.addWidget(self.lineEdit, 1, 1, 1, 1)
-        
-        self.label = QtWidgets.QLabel(self.centralwidget)
+
+        self.label=QtWidgets.QLabel(self.centralwidget)
         self.label.setAccessibleName("")
         self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label.setAlignment(QtCore.Qt.AlignRight |
+                                QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
         self.label.setObjectName("label")
         self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
         self.verticalLayout_6.addLayout(self.gridLayout_2)
-        
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
+
+        self.plainTextEdit=QtWidgets.QPlainTextEdit(self.centralwidget)
         self.plainTextEdit.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.ArrowCursor))
         self.plainTextEdit.setReadOnly(True)
         self.plainTextEdit.setPlaceholderText("")
         self.plainTextEdit.setObjectName("plainTextEdit")
         self.verticalLayout_6.addWidget(self.plainTextEdit)
-       
+
         MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar=QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        
-        self.pushButton.clicked.connect(self.start)
+
+        self.pushButton.clicked.connect(self.on_but1)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
+        _translate=QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "Add to database"))
-        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Enter a URL of hotel (from TripAdvisor only)"))
+        self.lineEdit.setPlaceholderText(_translate(
+            "MainWindow", "Enter a URL of hotel (from TripAdvisor only)"))
         self.label.setText(_translate("MainWindow", "City:"))
 
-    class worker(QRunnable):
-        
+    def on_but1(self):
+        try:
+            self.thread2=QThread2()
+            self.thread2.sig2.connect(self.on_info)
+            self.thread2.start()
+        except:
+            self.plainTextEdit.clear()
+            lineURL=self.lineEdit.text()
+            self.thread1=QThread1()
+            self.sig.connect(self.thread1.on_source)
+            self.sig.emit(lineURL)
+            self.thread1.start()
+            self.thread1.sig1.connect(self.on_info)
+
+    def on_info(self, info):
+        self.plainTextEdit.appendPlainText(str(info))
+
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    app=QtWidgets.QApplication(sys.argv)
+    MainWindow=QtWidgets.QMainWindow()
+    ui=Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-

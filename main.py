f6f2a23a0d676936cdd08fd7e7a84c22bfe54bb5
Kim Gabon <45309584+KimGabs@users.noreply.github.com>
2020-04-30 07:04:51 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -2,35 +2,36 @@ import requests
 from codes.url_parser import *
 from codes.Filter_Reviews import *
 from data_Conversion import *
+import time
 
 ip = requests.get('http://icanhazip.com').text
 
-start_urls = [
-    'https://www.tripadvisor.com.ph/Hotel_Review-g298573-d306018-Reviews-Diamond_Hotel_Philippines-Manila_Metro_Manila_Luzon.html'
-]
-
-
-def start():
-    global results
-    for url in start_urls:
-        print('Current IP:', ip)
-        p = ParseTripAdvisor(20) # (#)<-- Number of reviews to limit
-        check = p.parse(url, p.get_soup(url))
-        if check == True:
-            pass
-        else:
-            df = to_DataFrame(results)  # Convert list to DataFrame
-            del results[:]
-            df = filter_review(df, p.hotel_name)  # Divide the review sentences from the DataFrame
-            saveto_sql(df, p.hotel_name)  # <---- save pandas to sql
-            saveto_csv(df, p.hotel_name)  # <---- save pandas to csv
-        
-        # hotel_data_tocsv(df)
-        
+url = 'https://www.tripadvisor.com.ph/Hotel_Review-g298573-d5037302-Reviews-V_Hotel-Manila_Metro_Manila_Luzon.html'
 
 
 def main():
-    start()
+    global results
+    print('\nCurrent IP:', ip)
+    city = input('City: ').title()
+    p = ParseTripAdvisor(20, city)
+    print('Collecting soup...\n')
+    time.sleep(2)
+    p.get_soup(url)
+    p.parse(url)
+    if p.HotelExist() == True:
+        print("File ", p.hotelName, " already exists and is readable\n")
+        return
+    else:
+        print("The file does not exist...\n" + "Creating new file...\n")
+        df = to_DataFrame(results)  # Convert list to DataFrame
+        del results[:]
+        print('Filtering review...\n')
+        time.sleep(2)
+        df = filter_review(df, p.hotelName, city)  # Divide the review sentences from the DataFrame
+        print('Saving to sql and csv...\n')
+        time.sleep(1)
+        SetSave(df, p.hotelName, p.city, p.address)  # <---- save pandas to sql and csv
+        print(str(p.hotelName) + " Dataframe saved to sql and csv successfully!")
 
 
 if __name__ == '__main__':
